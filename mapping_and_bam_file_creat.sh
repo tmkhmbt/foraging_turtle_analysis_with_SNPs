c1c75192555ed156c63c9b2b0e3987f06b150e58
TomokoHamabata <119845218+TomokoHamabata@users.noreply.github.com>
2023-01-20 10:48:04 +0900

    Create mapping_and_bam_file_creat.sh

---
+++ mapping_and_bam_file_creat.sh
@@ -0,0 +1,11 @@
+#reference genome used 
+REF=/path/to/GCF_015237465.2_rCheMyd1.pri.v2_genomic.fna
+
+cd /path/to/all_quality_filtered_fastq_files/
+
+while read line 
+do
+bwa mem -t 8 -M $REF $line\_pair_R1.fastq $line\_pair_R2.fastq |\
+samtools view -b -q 20  -f 0x0002  -F 0x0004  -F 0x0008  -T $REF - | \
+samtools sort -T $NAME\.tmp - > /path/to/bams/$line\.bam
+done < sample_list.txt
