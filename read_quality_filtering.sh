a2eb2e272377f58c987c437f0e8ef62fb496cb21
TomokoHamabata <119845218+TomokoHamabata@users.noreply.github.com>
2023-01-20 10:45:46 +0900

    Create read_quality_filtering.sh

---
+++ read_quality_filtering.sh
@@ -0,0 +1,22 @@
+#quality filtering of raw reads and trimming of reads including adapter sequence
+#fastq_file_list.txt include the sample name in each line
+
+ADAPTER=/path/to/MIGadapter.fasta
+
+cd /path/to/raw_read_files/
+
+cat fastq_file_list.txt | while read line
+do
+  col1=`echo ${line} | cut -d '_' -f 1`
+  col2=`echo ${line} | cut -d '_' -f 2`
+java -Xmx8g -jar ~/bin/Trimmomatic-0.35/trimmomatic-0.35.jar \
+     PE \
+     -threads 28 \
+     -phred33 \
+     ${col1}\_${col2}\_L001_R1_001.fastq.gz ${col1}\_${col2}\_L001_R2_001.fastq.gz ${col1}\_pair_R1.fastq ${col1}\_unpair_R1.fastq ${col1}\_pair_R2.fastq ${col1}\_unpair_R2.fastq \
+     ILLUMINACLIP:$ADAPTER:2:30:10 \
+     SLIDINGWINDOW:4:15 \
+     CROP:77 \
+     HEADCROP:6 \
+     MINLEN:71
+done
