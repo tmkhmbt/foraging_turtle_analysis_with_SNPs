0340fb9f50b89d3f2ddee81d17bd9e7522dc6515
TomokoHamabata <119845218+TomokoHamabata@users.noreply.github.com>
2023-01-20 10:51:07 +0900

    Create origin_known_48sample_populations.sh

---
+++ origin_known_48sample_populations.sh
@@ -0,0 +1,7 @@
+
+#
+
+populations -P /path/to/bams/ -M /path/to/origin_known_48samples_3regions_map.txt \
+            -O /path/to/output/  --min_maf 0.05 -r 0.8 -p 2 --fstats --genepop --vcf
+            
+            
